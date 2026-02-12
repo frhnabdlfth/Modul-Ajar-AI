4f7de85129779a4623c16a28811d223df041d2a8
frhnabdlfth <frhnabdlfth270901@gmail.com>
2026-02-13 00:05:07 +0700

    Create page.tsx with Home and Dashboard components

---
+++ frontend/app/page.tsx
@@ -0,0 +1,13 @@
+import { Home } from './components/Home';
+import { Dashboard } from './components/Dashboard';
+
+const Page = () => {
+    return (
+        <div>
+            <Home />
+            <Dashboard />
+        </div>
+    );
+};
+
+export default Page;
